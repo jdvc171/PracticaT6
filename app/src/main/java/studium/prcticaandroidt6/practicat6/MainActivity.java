6b7f5f9d59686050340f9fd942601823b7c55d72
juan diego <jdvc171@hotmail.com>
2019-01-28 13:59:39 +0100

    Pantalla principal:
    Aparecerá un menú con dos opciones: "Personas" y "Bloc de notas" que al ser pulsadas se abrirán las correspondientes Activity.
    
    El Layout deberá mostrar dos grandes fotos, una mostrando la "silueta de una persona" y la otra la de un "bloc de notas", que al ser pulsadas se abrirán las correspondientes Activity, tal como si lo hiciéramos sobre el menú de opciones.

--- app/src/main/java/studium/prcticaandroidt6/practicat6/MainActivity.java
+++ app/src/main/java/studium/prcticaandroidt6/practicat6/MainActivity.java
@@ -1,13 +1,69 @@
 package studium.prcticaandroidt6.practicat6;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgPer,imgBlc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgPer = findViewById(R.id.imgPersonas);
+        imgBlc = findViewById(R.id.imgBloc);
+
+        imgPer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        contactos.class);
+                //Iniciamos la nueva actividad
+                startActivity(intent);
+            }
+        });
+
+        imgBlc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        bloc.class);
+                //Iniciamos la nueva actividad
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mnu_mainactivity,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mnuItemPersonas:
+                Intent intent = new Intent(MainActivity.this,
+                        contactos.class);
+                //Iniciamos la nueva actividad
+                startActivity(intent);
+                break;
+            case R.id.mnuItemBloc:
+                Intent intent2 = new Intent(MainActivity.this,
+                        bloc.class);
+                //Iniciamos la nueva actividad
+                startActivity(intent2);
+                break;
+        }
+
+        return true;
     }
 }
---
+++ app/src/main/java/studium/prcticaandroidt6/practicat6/bloc.java
@@ -0,0 +1,13 @@
+package studium.prcticaandroidt6.practicat6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bloc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bloc);
+    }
+}
