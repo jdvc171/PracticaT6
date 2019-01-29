35209344f10bdb8174eacd5b7d034670eaca49b0
juan diego <jdvc171@hotmail.com>
2019-01-29 22:27:39 +0100

    Pantalla del bloc:
    Se mostrará un área de texto en el que se podrá escribir cualquier texto multilínea.
    
    Sobre la barra de la Activity deberá aparecer un icono que permita guardar el texto como preferencia.
    
    Cada vez que se abra de nuevo esta App y se acceda al bloc, deberá mostrarse el último texto almacenado.
    
    Más strings y traducción a 2 idiomas más.

--- app/src/main/java/studium/prcticaandroidt6/practicat6/bloc.java
+++ app/src/main/java/studium/prcticaandroidt6/practicat6/bloc.java
@@ -1,13 +1,51 @@
 package studium.prcticaandroidt6.practicat6;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class bloc extends AppCompatActivity {
 
+    final String fichero = "";
+    SharedPreferences prefs;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bloc);
+        prefs = getSharedPreferences(fichero, Context.MODE_PRIVATE);
+        editText = findViewById(R.id.EditText);
+        editText.setText(prefs.getString("texto",""));
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mnu_bloc,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mnuItemBloc:
+                preferences();
+                Toast.makeText(bloc.this,"Nota guardada",Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return true;
+    }
+
+    public void preferences (){
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("texto",editText.getText().toString());
+        editor.commit();
+
     }
 }
